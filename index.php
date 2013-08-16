43c8149a908c03d1ef245fd4e67502a33abb8e9d
Howard Noz <howardcnoz+git@gmail.com>
2013-08-15 20:49:29 -0700

    Adds form (first commit)

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title>Google Form</title>
+</head>
+
+<body>
+
+<div>
+	<iframe src="https://docs.google.com/forms/d/1oXRMRTe_1h-cxRC9A8Wjk97RXmcXjqRIJixvPlNBSMg/viewform?embedded=true" height="1000" width="600" frameborder="0" marginwidth="0" marginheight="0"></iframe>
+</div>
+
+</body>
+</html>
