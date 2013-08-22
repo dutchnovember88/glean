b8915654d32af2c006530294a37d935382787f63
Howard Noz <howardcnoz+git@gmail.com>
2013-08-21 17:38:38 -0700

    Adds link to eval page.

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 <body>
 
 <div>
-	<h1>Example to work with</h1>
+	<a href="eval.php">go to eval</a>
+</div>
+<br><br>
+<div>
 	<iframe src="https://docs.google.com/forms/d/1oXRMRTe_1h-cxRC9A8Wjk97RXmcXjqRIJixvPlNBSMg/viewform?embedded=true" height="1000" width="600" frameborder="0" marginwidth="0" marginheight="0"></iframe>
 </div>
 
