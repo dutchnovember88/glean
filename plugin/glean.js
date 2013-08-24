6cce567d6ad08ca0ebfe00d2252c6c385c398310
dutchnovember88 <howardcnoz+github@gmail.com>
2013-08-23 17:04:27 -0700

    Shortcode version of wordpress plugin.

--- plugin/glean.js
+++ plugin/glean.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function ($) {
-
+	var target = "#"+jsParams.unique;
 	var sheet =jsParams.sheet;
-	$('#signups').sheetrock({
+	
+	$(target).sheetrock({
 		url: sheet,
 		sql: "select B, C order by A"
 		//,chunkSize: 20
--- plugin/glean.php
+++ plugin/glean.php
@@ -1,42 +1,47 @@
 <?php
 /**
  * @package Glean Plugin
- * @version 1.0
+ * @version 1.1
  */
 /*
 Plugin Name: Glean
 Plugin URI: http://wordpress.org/plugins/
-Description: This will hopefully take the information you want out of a google spreadsheet you have access to and display it.
+Description: Plugin to extract the information you want out of a google spreadsheet you have access to. Includes short code.
 Author: Howard Noz
 Version: 1.0
-Author URI: http://www.howardnoz.com
+Author URI: 
 */
 
-// add css
-add_action( 'wp_enqueue_scripts', 'add_css' );
-function add_css() {
-	wp_register_style('glean_css', plugins_url( '/glean.css' , __FILE__ ));
-	wp_enqueue_style('glean_css');
-}
+// shortcode
+function glean_short( $atts ) {
+	extract( shortcode_atts( array(
+		'sheet' => 'url here',
+	), $atts ) );
+	
+	$find = array("/.*[?]/", "/[^a-zA-Z0-9]/");
+	$replace = array("","");
+	$sheet = "{$sheet}";
+	$unique = preg_replace($find, $replace, $sheet);
+	
+	$params =array( 
+		'unique' => $unique,
+		'sheet' => $sheet ,
+	);
+	wp_register_script('glean_js', plugins_url( '/glean.js' , __FILE__ ), array( 'jquery' ));
+	wp_enqueue_script('glean_js');
+	wp_localize_script('glean_js', 'jsParams', $params);
 
-// add js
-add_action( 'wp_enqueue_scripts', 'glean_js' );
+	return "<table id=\"".$unique."\" cellspacing=\"0\" cellpadding=\"4\"></table>";
+}
+add_shortcode( 'display_response', 'glean_short' );
+
+// javascript
 function glean_js() {
 	wp_enqueue_script(
 		'jquery.sheetrock',
 		plugins_url( '/jquery.sheetrock.js' , __FILE__ ),
 		array( 'jquery' )
 	);
-	
-	wp_register_script(
-		'glean_js',
-		plugins_url( '/glean.js' , __FILE__ ),
-		array( 'jquery' )
-	);
-	wp_enqueue_script('glean_js');
-	$params =array(
-	'sheet' => "url here"
-	);
-	wp_localize_script('glean_js', 'jsParams', $params);
 }
+add_action( 'wp_enqueue_scripts', 'glean_js' );
 ?>
